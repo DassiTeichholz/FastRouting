f8fa916274b9cf5a979b86e2de51864342ade94d
Dassi Teichholz <116339277+DassiTeichholz@users.noreply.github.com>
2023-02-13 22:25:42 +0200

    dijkstra

--- FastRouting.Services/Services/Dijkstra.cs
+++ FastRouting.Services/Services/Dijkstra.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System;
 using System.Collections.Generic;
+using FastRouting.Common.DTO;
+using FastRouting.Repositories.Entities;
+using FastRouting.Repositories.Interfaces;
 
 
 namespace FastRouting.Services.Services
@@ -15,290 +18,74 @@ namespace FastRouting.Services.Services
 
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.Metrics;
     using System.Linq;
+    using System.Xml.Linq;
 
-    public class Dijkstra
+    public static class Dijkstra
     {
-        private List<LocationsDTO> locations;
-        private List<IntersectionsDTO> intersections;
-        private List<EdgesDTO> edges;
-
-        public Dijkstra(List<LocationsDTO> locations, List<IntersectionsDTO> intersections, List<EdgesDTO> edges)
+        public class internalObject
         {
-            this.locations = locations;
-            this.intersections = intersections;
-            this.edges = edges;
+            public int idCoordinate;
+            public double distance;
+
+            public internalObject(int idCoordinate, double distance)
+            {
+                this.idCoordinate=idCoordinate;
+                this.distance=distance;
+            }
         }
 
-        public List<LocationsDTO> ShortestPath(int source, int destination)
+        public class Vertex
         {
-            Dictionary<int, double> distance = new Dictionary<int, double>();
-            Dictionary<int, int> previous = new Dictionary<int, int>();
-            List<int> unvisited = new List<int>();
+            //private int vertexNum;
+            private CoordinateDTO coordinate;
+            private LocationTypesDTO LocationTypes;
+            private string name;
+            private List<internalObject> internalObject;
+            //private int num;
 
-            foreach (var location in locations)
+            public Vertex(CoordinateDTO coordinate, LocationTypesDTO LocationTypes,string name)
             {
-                distance[location.Id] = double.MaxValue;
-                previous[location.Id] = -1;
-                unvisited.Add(location.Id);
+                this.coordinate = coordinate;
+                this.LocationTypes = LocationTypes;
+                this.name = name;
+                this.internalObject = new List<internalObject>();
             }
-            distance[source] = 0;
-
-            while (unvisited.Count != 0)
+            public Vertex(CoordinateDTO coordinate)
             {
-                int minId = unvisited.OrderBy(x => distance[x]).First();
-                unvisited.Remove(minId);
+                this.coordinate = coordinate;
+                this.LocationTypes = null; ;
+                this.name = null;
 
-                foreach (var edge in edges.Where(x => x.LocationIdA == minId || x.LocationIdB == minId))
-                {
-                    int neighborId = edge.LocationIdA == minId ? edge.LocationIdB : edge.LocationIdA;
-                    if (unvisited.Contains(neighborId))
-                    {
-                        double newDistance = distance[minId] + edge.Distance;
-                        if (newDistance < distance[neighborId])
-                        {
-                            distance[neighborId] = newDistance;
-                            previous[neighborId] = minId;
-                        }
-                    }
-                }
             }
-
-            List<LocationsDTO> result = new List<LocationsDTO>();
-            int current = destination;
-            while (current != -1)
-            {
-                result.Add(locations.First(x => x.Id == current));
-                current = previous[current];
-            }
-            result.Reverse();
-            return result;
         }
+        public static List<Vertex> Dijkstra(int startId, int endId, List<LocationsDTO> locations, List<IntersectionsDTO> intersections, List<EdgesDTO> edges)
+        {
+
+            int[] parent = new int[locations.Count+intersections.Count];
+            bool[] isVisit = new bool[locations.Count+intersections.Count];
+            double[] distance = new double[locations.Count+intersections.Count];
+            Vertex[] graph = new Vertex[locations.Count+intersections.Count];
+            double[] heapArray = new double[locations.Count+intersections.Count];
+
+            foreach (Vertex edge in graph) 
+            {
+                int index = IntStream.range(0, persons.length)
+                                     .filter(i->persons[i].id == 5)
+                                     .findFirst()
+                                     .orElse(-1);
+            }
+
+
+
+
+
+
+
+        }
+
+
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //using System;
-    //using System.Collections.Generic;
-
-    //public class Dijkstra
-    //{
-
-
-
-    //    //    public List<object> DijkstraShortestPath(List<LocationsDTO> locations, List<IntersectionsDTO> intersections, int sourceId, int destinationId)
-    //    //    {
-    //    //        int n = locations.Count + intersections.Count;
-    //    //        double[] distances = new double[n];
-    //    //        int[] previous = new int[n];
-    //    //        bool[] visited = new bool[n];
-
-    //    //        for (int i = 0; i < n; i++)
-    //    //        {
-    //    //            distances[i] = double.MaxValue;
-    //    //            previous[i] = -1;
-    //    //        }
-
-    //    //        distances[sourceId] = 0;
-
-    //    //        for (int i = 0; i < n; i++)
-    //    //        {
-    //    //            int current = GetNextNode(distances, visited);
-    //    //            visited[current] = true;
-
-    //    //            if (current == destinationId)
-    //    //            {
-    //    //                break;
-    //    //            }
-
-    //    //            foreach (var edge in locations[current].Transitions.Edges)
-    //    //            {
-    //    //                int neighbor = edge.LocationIdB;
-    //    //                double weight = edge.Distance;
-
-    //    //                if (visited[neighbor])
-    //    //                {
-    //    //                    continue;
-    //    //                }
-
-    //    //                if (distances[current] + weight < distances[neighbor])
-    //    //                {
-    //    //                    distances[neighbor] = distances[current] + weight;
-    //    //                    previous[neighbor] = current;
-    //    //                }
-    //    //            }
-    //    //        }
-
-    //    //        List<object> path = new List<object>();
-    //    //        int currentNode = destinationId;
-
-    //    //        while (currentNode != -1)
-    //    //        {
-    //    //            if (currentNode < locations.Count)
-    //    //            {
-    //    //                path.Add(locations[currentNode]);
-    //    //            }
-    //    //            else
-    //    //            {
-    //    //                path.Add(intersections[currentNode - locations.Count]);
-    //    //            }
-    //    //            currentNode = previous[currentNode];
-    //    //        }
-
-    //    //        path.Reverse();
-
-    //    //        return path;
-    //    //    }
-
-    //    //    private int GetNextNode(double[] distances, bool[] visited)
-    //    //    {
-    //    //        int nextNode = -1;
-    //    //        double minDistance = double.MaxValue;
-
-    //    //        for (int i = 0; i < distances.Length; i++)
-    //    //        {
-    //    //            if (visited[i])
-    //    //            {
-    //    //                continue;
-    //    //            }
-
-    //    //            if (distances[i] < minDistance)
-    //    //            {
-    //    //                minDistance = distances[i];
-    //    //                nextNode = i;
-    //    //            }
-    //    //        }
-
-    //    //        return nextNode;
-    //    //    }
-
-
-
-
-
-
-    //    //}
-
-
-
-
-
-    //    public List<object> DijkstraShortestPath(List<LocationsDTO> locations, List<IntersectionsDTO> intersections, int sourceId, int destinationId)
-    //    {
-    //        int n = locations.Count + intersections.Count;
-    //        Dictionary<int, double> distances = new Dictionary<int, double>();
-    //        Dictionary<int, int> previous = new Dictionary<int, int>();
-    //        HashSet<int> visited = new HashSet<int>();
-
-    //        for (int i = 0; i < n; i++)
-    //        {
-    //            int id;
-    //            if (i < locations.Count)
-    //            {
-    //                id = locations[i].Coordinate.Id;
-    //            }
-    //            else
-    //            {
-    //                id = intersections[i - locations.Count].Coordinate.Id;
-    //            }
-    //            distances[id] = double.MaxValue;
-    //            previous[id] = -1;
-    //        }
-
-    //        distances[sourceId] = 0;
-
-    //        for (int i = 0; i < n; i++)
-    //        {
-    //            int current = GetNextNode(distances, visited);
-    //            visited.Add(current);
-
-    //            if (current == destinationId)
-    //            {
-    //                break;
-    //            }
-
-    //            LocationsDTO currentLocation = locations.Find(loc => loc.Id == current);
-    //            foreach (var edge in currentLocation.Transitions.Edges)
-    //            {
-    //                int neighbor = edge.LocationIdB;
-    //                double weight = edge.Distance;
-
-    //                if (visited.Contains(neighbor))
-    //                {
-    //                    continue;
-    //                }
-
-    //                if (distances[current] + weight < distances[neighbor])
-    //                {
-    //                    distances[neighbor] = distances[current] + weight;
-    //                    previous[neighbor] = current;
-    //                }
-    //            }
-    //        }
-
-    //        List<object> path = new List<object>();
-    //        int currentNode = destinationId;
-
-    //        while (currentNode != -1)
-    //        {
-    //            LocationsDTO location = locations.Find(loc => loc.Id == currentNode);
-    //            if (location != null)
-    //            {
-    //                path.Add(location);
-    //            }
-    //            else
-    //            {
-    //                IntersectionsDTO intersection = intersections.Find(inter => inter.Id == currentNode);
-    //                path.Add(intersection);
-    //            }
-    //            currentNode = previous[currentNode];
-    //        }
-
-    //        path.Reverse();
-
-    //        return path;
-    //    }
-
-    //    private int GetNextNode(Dictionary<int, double> distances, HashSet<int> visited)
-    //    {
-    //        int nextNode = -1;
-    //        double minDistance = double.MaxValue;
-
-    //        foreach (var item in distances)
-    //        {
-    //            int node = item.Key;
-    //            if (visited.Contains(node))
-    //            {
-    //                continue;
-    //            }
-
-    //            if (item.Value < minDistance)
-    //            {
-    //                minDistance = item.Value;
-    //                nextNode = node;
-    //            }
-    //        }
-
-    //        return nextNode;
-    //    }
-
-    //}
 }
